b9694d8813edc7930234c33f69e27ad8e137e5bc
HiickFG <henriq_fg@hotmail.com>
2025-01-01 03:13:05 +0000

    Commiting changes

---
+++ hiickfg.github.io/src/App.js
@@ -0,0 +1,16 @@
+import MatrixCanvas from "./components/MatrixCanvas/MatrixCanvas";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <MatrixCanvas />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
