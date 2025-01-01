91bb2e5f94448c946d23f93e11963929503cff1f
HiickFG <henriq_fg@hotmail.com>
2025-01-01 00:40:40 +0000

    Set up React app for GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my Portfolio!</h1>
+        <p>Hi, I'm Henrique. This is my first react project!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
