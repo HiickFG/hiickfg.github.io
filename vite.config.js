90c65a9d6d543d9a3e4b955a42da2a637a411e4b
HiickFG <henriq_fg@hotmail.com>
2025-01-17 00:25:15 +0000

    Deploy site to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'docs'
+  },
   css: {
     preprocessorOptions: {
       less: {
