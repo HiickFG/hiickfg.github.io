e184abfeddc79f7615e98cf3ce141ebd0cd2cd44
HiickFG <henriq_fg@hotmail.com>
2025-01-17 00:06:33 +0000

    Attempt to fix issue with loading module

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hiickfg.github.io/',
+  base: '/',
   css: {
     preprocessorOptions: {
       less: {
