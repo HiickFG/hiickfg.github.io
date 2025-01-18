53e1779700ce1ae5e39556084770c4ed61823fe5
HiickFG <henriq_fg@hotmail.com>
2025-01-18 20:05:33 +0000

    Adding HeroUI for components usage in dark mode, which includes TailwindCSS and PostCSS. Also added react-icons package to be able to load icons as React components directly, facilitating implementations.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-
 // Styles
+import './styles/tailwind.css';
 import './styles/main.less';
 
 // Scripts
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app.jsx';
+import { HeroUIProvider } from '@heroui/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <main className="dark text-foreground bg-background">
+        <App />
+      </main>
+    </HeroUIProvider>
   </StrictMode>,
-)
+);
